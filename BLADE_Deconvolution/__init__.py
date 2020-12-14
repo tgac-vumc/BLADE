f4f747278e72e5d9efe47279edfe67cce44a7db8
Kim <y.kim1@hpc-pat01.maas>
2020-12-14 09:01:53 -0500

    Update Python package and demo script

--- BLADE_Deconvolution/__init__.py
+++
@@ -1,1 +0,0 @@
-__all__ = ["BLADE_framework", "BLADE"]
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="BLADE-Deconvolution",
-    version="0.0.4",
+    version="0.0.5",
     author="Yongsoo Kim",
     author_email="anoyaro84@gmail.com",
     description="BLADE (Bayesian Log-normAl DEconvolution)",
